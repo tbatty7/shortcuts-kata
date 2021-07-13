144f58c704fb9e75aaa96a24ba3fe0c6e2e22134
Tim B <tbatty7@gmail.com>
2021-07-13 14:29:15 -0400

    initializr commit

---
+++ src/main/java/com/battybuilds/shortcutskata/ShortcutsKataApplication.java
@@ -0,0 +1,13 @@
+package com.battybuilds.shortcutskata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortcutsKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortcutsKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/battybuilds/shortcutskata/ShortcutsKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.battybuilds.shortcutskata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortcutsKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
